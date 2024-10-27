e54e6c629cd85a947026fbfa3e145869463a1d2a
Eva <59949752+Evalin0316@users.noreply.github.com>
2024-10-27 18:10:26 +0800

    modify setting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,8 +30,8 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hant',
+    locales: ['zh-Hant'],
   },
 
   presets: [
@@ -87,7 +87,7 @@ const config = {
             label: 'Tutorial',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
-          {to: '/hello', label: 'Hello', position: 'left'},
+          // {to: '/hello', label: 'Hello', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
